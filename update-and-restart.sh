386d56e504e401d724e5d517628c076588d3ed8f
Ali Can Kuştemur <alican.kustemur@gmail.com>
2022-11-16 00:05:45 +0300

    Add service and script

---
+++ update-and-restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd /home/exporter/eur-currency-prometheus-exporter
+
+kill -9 $(ps aux|grep eur-currency-prometheus-exporter | awk 'NR==1{print $2}') || true
+git reset origin
+git pull
+rm -rf ./eur-currency-prometheus-exporter
+go build .
+nohup ./eur-currency-prometheus-exporter &
+
+popd +1
