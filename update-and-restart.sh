d7e4c442f163ee49a913e1fe8fe60c108c479347
Ali Can Kuştemur <alican.kustemur@gmail.com>
2022-11-16 00:08:01 +0300

    Add --hard flag to git reset

--- update-and-restart.sh
+++ update-and-restart.sh
@@ -3,7 +3,7 @@
 pushd /home/exporter/eur-currency-prometheus-exporter
 
 kill -9 $(ps aux|grep eur-currency-prometheus-exporter | awk 'NR==1{print $2}') || true
-git reset origin
+git reset origin --hard
 git pull
 rm -rf ./eur-currency-prometheus-exporter
 go build .
