f10478961bff6c6c6fa7abcafaea22109beb5df1
Ali Can Kuştemur <alican.kustemur@gmail.com>
2024-07-14 17:46:46 +0300

    Fix kuveytturk eur metric

--- go.mod
+++ go.mod
@@ -20,6 +20,9 @@ require (
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
--- kuveytturk/eur.go
+++ kuveytturk/eur.go
@@ -1,14 +1,14 @@
 package kuveytturk
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/tidwall/gjson"
 )
 
 var currentCurrencyMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
@@ -19,35 +19,39 @@ var currentCurrencyMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 func setCurrentEur() {
 
 	for {
-
-		req, err := http.NewRequest("GET", "https://www.kuveytturk.com.tr/finans-portali/", nil)
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", "https://www.kuveytturk.com.tr/ck0d84?C24AD4C0FDA76C73081889B634A8C039", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		res, err := http.DefaultClient.Do(req)
-
-		doc, err := goquery.NewDocumentFromReader(res.Body)
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+		req.Header.Set("Connection", "keep-alive")
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Cookie", "NSC_WTSW-XFCTJUF-TTM=ffffffffaf1c1ca845525d5f4f58455e445a4a42378b; TS01e71088=0176dcd71c45d828f601804554a1b3d6fe9b3c3039eb4cfc375a376e36d668b7912f2937bbc75a2f018d9a33dd6ffb1cd437388b626329fb18ea8d84a40431307dd6dc9c7e; _ga=GA1.3.1626640906.1720965814; _gid=GA1.3.482756998.1720965814")
+		req.Header.Set("Referer", "https://www.kuveytturk.com.tr/finans-portali")
+		req.Header.Set("Sec-Fetch-Dest", "empty")
+		req.Header.Set("Sec-Fetch-Mode", "cors")
+		req.Header.Set("Sec-Fetch-Site", "same-origin")
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+		req.Header.Set("X-Bone-Language", "TR")
+		req.Header.Set("X-Requested-With", "XMLHttpRequest")
+		req.Header.Set("sec-ch-ua", `"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"`)
+		req.Header.Set("sec-ch-ua-mobile", "?0")
+		req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+		resp, err := client.Do(req)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		defer res.Body.Close()
+		defer resp.Body.Close()
+		bodyText, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		var currentEur float64
-
-		doc.Find(".col-sm-6").Each(func(i int, s *goquery.Selection) {
-
-			if s.Find("h2").First().Text() == `EUR (Avrupa Para Birimi)` {
-				eurText := s.Find("p").First().Text()
-				eurText = strings.Replace(eurText, "\n", "", -1)
-				eurText = strings.Replace(eurText, " ", "", -1)
-				eurText = strings.Split(eurText, "Alış")[0]
-				eurText = strings.Replace(eurText, ",", ".", -1)
-
-				currentEur, err = strconv.ParseFloat(eurText, 64)
-			}
-		})
+		value := gjson.Get(string(bodyText), "2.BuyRate")
+		currentEur, err = strconv.ParseFloat(value.String(), 64)
 
 		currentCurrencyMetric.WithLabelValues().Set(currentEur)
 		time.Sleep(5 * time.Second)
